ed97deff60ebad9126379fa656ea1004513ce985
maria <mvbri10@gmail.com>
2021-08-21 15:43:58 -0400

    aclaracion

--- beez-website/_partials/scripts.php
+++
@@ -1,4 +0,0 @@
-<script src="_private/vendors/owl/jquery.min.js"></script>
-<script src="_private/vendors/owl/owl.carousel.min.js"></script>
-<script src="_private/js/global.js"></script>
-<script src="_private/js/blog.js"></script>
